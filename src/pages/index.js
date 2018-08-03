37e34a2541de1948da5ed8ba3026151cae42968f
Christopher Davis <chris@christopheralandavis.com>
2018-08-02 21:13:18 -0400

    Wow! You can commit from VSCode!

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Learn to design and code React apps.</h1>
     <p>I'm just learning how to write code in React!</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video.</Link>
   </div>
 )
 
